0e75b0d494cc873162b4a272ac62e7de4c4ab050
Robby Ranshous <rranshous@gmail.com>
2015-04-15 21:20:12 -0400

    deploy start + end, readme

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require 'sinatra'
 require 'twilio-ruby'
 
-MESSAGE = """
-.
+START_MESSAGE = """
+...
 This is a message for %{recipient} concerning a deployment.
 Team %{team_name} is about to deploy %{app_name}.
 If you find this news concerning please press 1 and stay on the line and you will be connected with the team.
+This message will repeat.
+"""
+
+END_MESSAGE = """
+...
+This is a message for %{recipient} concerning a deployment.
+Team %{team_name} has completed their deployment of their %{app_name}.
+If you want to congradulate them press 1 and stay on the line and you will be connected with the team.
 This message will repeat.
 """
 
@@ -15,36 +23,65 @@ account_sid = ENV['TWILIO_ACCOUNT_SID']
 auth_token = ENV['TWILIO_AUTH_TOKEN']
 $client = Twilio::REST::Client.new account_sid, auth_token
 
-get '/' do
-  return '''<form method="POST" action="/">
-      <label>What\'s the phone # this message should go to? <input type="text" name="target_phone"/></label>
-      <label>Who is going to receive this message? "This is a message for <input type="text" name="recipient"/>"</label>
-      <label>What is your (recignizable) team name? "This is a message from <input type="text" name="team_name"/>"</label>
-      <label>What app are you deploying? "The app is named <input type="text" name="app_name"/>"</label>
-      <label>What phone number should the recipient call if they need to follow up? "To reach me call <input type="text" name="callback_num"/>"</label>
+VIEW = '''<form method="POST">
+      <label>What\'s the phone # this message should go to? [ex| 3215554321] <input type="text" name="target_phone"/></label>
+      <label>Who is going to receive this message? [ex| Bob from B.E.E] "This is a message for <input type="text" name="recipient"/>"</label>
+      <label>What is your (recignizable) team name? [ex| I.F.S] "This is a message from <input type="text" name="team_name"/>"</label>
+      <label>What app are you deploying? [ex| Inventory App]  "We\'re about to deploy the <input type="text" name="app_name"/>"</label>
+      <label>What phone # should the recipient call if they need to follow up? "To reach me call <input type="text" name="callback_num"/>"</label>
       <label>
       <input type="submit"/>
     </form>
-  '''.gsub("\n","<br/>")
+'''.gsub("\n","<br/>")
+
+get '/' do
+  '''
+  <a href="/deploy_start">deploy start message</a>
+  <hr/>
+  <a href="/deploy_end">deploy end message</a>
+  '''
 end
 
-post '/' do
+get '/deploy_start' do
+  VIEW
+end
+
+post '/deploy_start' do
   @call = $client.calls.create(
     from: TWILIO_NUMBER,
     to: params["target_phone"],
-    url: callback_url(params)
+    url: start_callback_url(params)
+  )
+  "Call initiated
+  #{curl_command(request, params)}".gsub("\n","<br/>")
+end
+
+get '/deploy_end' do
+  VIEW
+end
+
+post '/deploy_end' do
+  @call = $client.calls.create(
+    from: TWILIO_NUMBER,
+    to: params["target_phone"],
+    url: end_callback_url(params)
   )
   "Call initiated
   #{curl_command(request, params)}".gsub("\n","<br/>")
 end
 
 helpers do
-  def callback_url params
-    url = "http://twimlets.com/menu?" + URI.encode_www_form({
-      Message: MESSAGE % Hash[params.to_a.map{|k,v|[k.to_sym,v]}],
+  def start_callback_url params
+    "http://twimlets.com/menu?" + URI.encode_www_form({
+      Message: START_MESSAGE % Hash[params.to_a.map{|k,v|[k.to_sym,v]}],
       "Options[1]": bridge_url(params)
     })
-    url
+  end
+  def end_callback_url params
+    "http://twimlets.com/menu?" + URI.encode_www_form({
+      Message: END_MESSAGE % Hash[params.to_a.map{|k,v|[k.to_sym,v]}],
+      "Options[1]": bridge_url(params)
+    })
   end
   def bridge_url params
     "http://twimlets.com/forward?" + URI.encode_www_form({
